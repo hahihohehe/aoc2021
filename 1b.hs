909ca05abc36154ae640e098b98d3d41c83624ea
hahiho <n.a@live.de>
2021-12-02 18:57:18 +0100

    Shorter representation for 1b

--- 1b.hs
+++ 1b.hs
@@ -6,11 +6,7 @@ main = do
   putStrLn $ show $ (countIncreases . slidingWindow . map read . words) contents
 
 countIncreases :: [Int] -> Int
-countIncreases (x:xs) = countIncreasesAcc xs 0 x where
-  countIncreasesAcc [] acc _ = acc
-  countIncreasesAcc (x:xs) acc last
-    | x > last = countIncreasesAcc xs (acc+1) x
-    | otherwise = countIncreasesAcc xs acc x
+countIncreases (x:xs) = snd $ foldl (\(last, acc) e -> if e > last then (e, acc+1) else (e, acc)) (x, 0) xs
 
 slidingWindow :: [Int] -> [Int]
 slidingWindow (a:b:xs) = reverse $ tail $ tail $ foldl (\(x1:x2:xx) e -> x2:e:(x1+x2+e):xx) [a,b] xs
