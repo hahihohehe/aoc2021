e03b1b53b4d1a6fc6b91eec34b2003ffb12a2720
hahiho <n.a@live.de>
2021-12-03 22:37:07 +0100

    Correct 3b

--- 3b.hs
+++ 3b.hs
@@ -4,17 +4,18 @@ main = do
   args <- getArgs
   contents <- readFile $ head args
   let gamma = (gammaRate . lines) contents
+  let eps = (epsilonRate . lines) contents
   putStrLn $ show $ gamma
-  putStrLn $ show $ epsilonRate gamma
-  putStrLn $ show $ (strToInt gamma) * (strToInt $ epsilonRate gamma)
-  let oxygenGeneratorRating = head $ applyFilters (oxFilters $ lines contents) $ lines contents
-  let co2ScrubberRating = head $ applyFilters (co2Filters $ lines contents) $ lines contents
+  putStrLn $ show $ eps
+  putStrLn $ show $ (strToInt gamma) * (strToInt $ eps)
+  let oxygenGeneratorRating = head $ filterRounds gammaRate $ lines contents
+  let co2ScrubberRating = head $ filterRounds epsilonRate $ lines contents
   putStrLn oxygenGeneratorRating
   putStrLn co2ScrubberRating
   putStrLn $ show $ (strToInt oxygenGeneratorRating) * (strToInt co2ScrubberRating)
 
 gammaRate :: [String] -> String
-gammaRate =  biasToChar . foldr biasChange (repeat 0)
+gammaRate =  biasToChar1 . foldr biasChange (repeat 0)
 
 biasChange (x:xs) (a:as) = changeBias x a : biasChange xs as
 biasChange [] _ = []
@@ -23,30 +24,28 @@ changeBias c acc
   | c == '1' = acc + 1
   | c == '0' = acc - 1
 
-biasToChar (x:xs)
-  | x > 0 = '1' : biasToChar xs
-  | x < 0 = '0' : biasToChar xs
-biasToChar [] = []
+biasToChar1 (x:xs)
+  | x >= 0 = '1' : biasToChar1 xs
+  | x < 0 = '0' : biasToChar1 xs
+biasToChar1 [] = []
+
+biasToChar0 (x:xs)
+  | x >= 0 = '0' : biasToChar0 xs
+  | x < 0 = '1' : biasToChar0 xs
+biasToChar0 [] = []
 
 strToInt = foldl (\acc x -> 2 * acc + read [x] :: Int) 0
 
-epsilonRate (x:xs)
-  | x == '1' = '0' : epsilonRate xs
-  | x == '0' = '1' : epsilonRate xs
-epsilonRate [] = []
+epsilonRate = biasToChar0 . foldr biasChange (repeat 0)
 
 isChar :: Int -> Char -> String -> Bool
 isChar index char str = str!!index == char
 
-oxFilters :: [String] -> [String -> Bool]
-oxFilters lines = map (\index -> isChar index ((gammaRate lines)!!index)) [0..(length $ head lines)-1]
+filterRounds :: ([String] -> String) -> [String] -> [String]
+filterRounds bitmapGen = filterRound 0 where
+  filterRound idx [x] = [x]
+  filterRound idx lines = filterRound (idx+1) $ myFilter (isChar idx ((bitmapGen lines)!!idx)) lines
 
-co2Filters :: [String] -> [String -> Bool]
-co2Filters lines = map (\index -> isChar index ((epsilonRate $ gammaRate lines)!!index)) [0..(length $ head lines)-1]
-
-applyFilters :: [a -> Bool] -> [a] -> [a]
-applyFilters (f:fs) list = applyFilters fs $ myFilter f list
-applyFilters _ [x] = [x]
 
 -- Filters until there is only one item left
 myFilter :: (a -> Bool) -> [a] -> [a]
