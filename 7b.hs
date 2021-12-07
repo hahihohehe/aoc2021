c6a13b8ce0447b4578881fa43545c309a82c1780
niklas <n.a@live.de>
2021-12-07 15:02:51 +0100

    7b

---
+++ 7b.hs
@@ -0,0 +1,20 @@
+import System.Environment
+import Data.List
+
+main = do
+  args <- getArgs
+  contents <- readFile $ head args
+  let l = lines contents
+  let num = parseInputs $ head l
+  putStrLn $ show $ minimum $ calcDist num
+
+parseInputs :: String -> [Int]
+parseInputs = map read . words . map comToSp where
+   comToSp ',' = ' '
+   comToSp x = x
+
+calcDist :: [Int] -> [Int]
+calcDist list = map (\x -> dist list x) [minimum list..maximum list]
+
+dist :: [Int] -> Int -> Int
+dist list x = (sum $ map (\e -> (abs (x-e)) * (abs (x-e) + 1) `div` 2) list) 
